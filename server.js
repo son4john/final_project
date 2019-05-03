4c4d7bb142c88f83e5a0eb2f9faeb9221f6609f5
son4john <49545823+son4john@users.noreply.github.com>
2019-05-03 00:21:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 console.log('Server-side code running');
-
+const http = require('http');
+const port= process.env.PORT || 3000
 const express = require('express');
-
 const app = express();
 
 // serve files from the public directory
 app.use(express.static('public'));
 
-// start the express web server listening on 8080
-app.listen(8080, () => {
+//const server = http.createServer((req, res) => {
+//res.statusCode = 200;
+//res.setHeader('Content-Type', 'text/html');
+//res.end(stringObj);
+//});
+//server.listen(port,() => {
+//console.log(`Server running at port `+port);
+//});
+
+// start the express web server listening on 8080 // change to port for example
+app.listen(port, () => {
   console.log('listening on 8080');
 });
 
